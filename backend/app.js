12ff2999b13568eaffaab1d52b8c3edbc237c25a
Adam Mikacich <adammikacich@gmail.com>
2022-06-14 17:20:25 -0700

    add bot structure

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,5 @@ import debugModule from 'debug';
 const debug = debugModule('app');
 const error = debugModule('error');
 
+import './services/bot/index.js';
 import './services/router/index.js';
---
+++ backend/services/bot/index.js
@@ -0,0 +1,6 @@
+import debugModule from 'debug';
+const debug = debugModule('bot');
+const error = debugModule('error');
+
+debug('connecting to bot');
+// connect here
